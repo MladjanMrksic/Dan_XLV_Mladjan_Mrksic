7fb52c033b6ea63d52799b307c82b54d1421f448
Dalish <Dalish@DESKTOP-S7S9PJT>
2020-07-13 10:59:24 +0200

    Add project files.

---
+++ Dan_XLV_Mladjan_Mrksic/App.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Dan_XLV_Mladjan_Mrksic
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+}
